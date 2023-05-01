35fe949b69e30ce399634ebd41e8f28470e15403
Mishi15 <122174407+Mishi15@users.noreply.github.com>
2023-05-02 00:40:35 +0530

    Create intmax_min.cpp

---
+++ intmax_min.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int x = INT_MAX; // TO FIND THE MAXIMUM DIGITS A INTEGER CAN STORE
+    int Y = INT_MIN; // TO FIND THE MINIMUM DIGIT A INTEGER CAN STORE
+    cout << x << Y;
+}
